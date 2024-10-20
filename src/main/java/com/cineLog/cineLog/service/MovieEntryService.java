0d5249904209cc1b3f16afa9b6e2907119f50da1
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2024-10-20 20:30:07 +0530

    Added Role Based Authorization for MovieEntityController using @PreAuthorize

--- src/main/java/com/cineLog/cineLog/service/MovieEntryService.java
+++ src/main/java/com/cineLog/cineLog/service/MovieEntryService.java
@@ -1,10 +1,12 @@
 package com.cineLog.cineLog.service;
 
 import com.cineLog.cineLog.entity.MovieEntity;
+import com.cineLog.cineLog.entity.UserEntity;
 import com.cineLog.cineLog.repository.MovieEntryRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +17,9 @@ public class MovieEntryService {
 
     @Autowired
     private MovieEntryRepo movieEntryRepo;
+
+    @Autowired
+    private  UserEntryService userEntryService;
 
     public void saveEntry(MovieEntity movieEntity) {
         movieEntryRepo.save(movieEntity);
@@ -30,5 +35,25 @@ public class MovieEntryService {
 
     public void deleteById(ObjectId id) {
         movieEntryRepo.deleteById(id);
+    }
+
+    @Transactional
+    public boolean deleteById(ObjectId id, String username) {
+        boolean removed = false;
+
+        try {
+            UserEntity user = userEntryService.findByusername(username);
+            removed = user.getReviewEntities().removeIf(x -> x.getId().equals(id));
+            if (removed) {
+                userEntryService.saveEntry(user);
+                movieEntryRepo.deleteById(id);
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new RuntimeException("An error occurred while deleting the entry", e);
+        }
+        return removed;
     }
 }
